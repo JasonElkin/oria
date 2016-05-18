a3103e1c5ba77d046d41d8f020ec64f339de099c
Jason Elkin <jason@semantic.co.uk>
2016-05-18 21:13:06 +0100

    Oi

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -89,7 +89,7 @@ function oria_posted_on() {
 
 	$byline = sprintf(
 		esc_html_x( '%s', 'post author', 'oria' ),
-		'<span class="author vcard"><i class="fa fa-user" aria-hidden="true"><i/> <a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard"><i class="fa fa-user" aria-hidden="true"></i> <a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
 	echo '<span class="posted-on"><i class="fa fa-calendar" aria-hidden="true"></i> ' . $posted_on . '</span> <span class="byline"> ' . $byline . '</span>';
